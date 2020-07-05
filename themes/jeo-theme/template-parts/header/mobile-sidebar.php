1e83dc9a210adf1bbb1c1f71d86e75efeeed34db
pame <pamela.rosales@pucp.pe>
2020-07-05 00:37:36 -0300

    css for sidebar menu

--- themes/jeo-theme/template-parts/header/mobile-sidebar.php
+++ themes/jeo-theme/template-parts/header/mobile-sidebar.php
@@ -15,13 +15,28 @@ if ( newspack_is_amp() ) : ?>
 	<aside id="mobile-sidebar-fallback" class="mobile-sidebar">
 		<button class="mobile-menu-toggle">
 			<?php echo wp_kses( newspack_get_icon_svg( 'close', 20 ), newspack_sanitize_svgs() ); ?>
-			<?php esc_html_e( 'Close', 'newspack' ); ?>
 		</button>
 <?php endif; ?>
 
 		<?php
 
 		newspack_primary_menu();
+		
+		$button_url = get_theme_mod('discovery_button_link'); 
+
+		
+		if (!empty($button_url)): ?>
+			<ul class="main-menu">
+				<li class="menu-item menu-item-type-post_type menu-item-object-page">
+					<a href="<?= $button_url ?>" class="discovery-link">
+						DISCOVERY
+					</a>
+				</li>
+			</ul>
+		<?php endif;
+
+		newspack_tertiary_menu();
+
 
 		newspack_social_menu_header();
 		?>
