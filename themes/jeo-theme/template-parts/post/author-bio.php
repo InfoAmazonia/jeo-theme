7b82de2107290733bc5a470f9e53b0a212c613a4
pame <pamela.rosales@pucp.pe>
2020-07-09 00:47:23 -0300

    fix in author-bio.php due to new version of newspack-theme. fix css in featured image

--- themes/jeo-theme/template-parts/post/author-bio.php
+++ themes/jeo-theme/template-parts/post/author-bio.php
@@ -42,18 +42,11 @@ if (function_exists('coauthors_posts_links') && is_single() && !empty(get_coauth
 					<?= __('About the author', 'jeo') ?>
 				</div>
 				<?php endif; ?>
-
-				<?php
-				if (!newspack_is_active_style_pack('style-4') && $author_avatar) {
-					echo wp_kses($author_avatar, newspack_sanitize_avatars());
-				}
-				?>
-
 				<div class="author-bio-text">
 
 					<div class="author-bio-header">
 						<?php
-						if (newspack_is_active_style_pack('style-4') && $author_avatar) {
+						if ($author_avatar) {
 							echo wp_kses($author_avatar, newspack_sanitize_avatars());
 						}
 						?>
@@ -124,27 +117,15 @@ if (function_exists('coauthors_posts_links') && is_single() && !empty(get_coauth
 	<div class="author-bio">
 
 		<?php
-		if (!newspack_is_active_style_pack('style-4')) {
 			$author_avatar = get_avatar(get_the_author_meta('ID'), 80);
 			if ($author_avatar) {
 				// phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
 				echo $author_avatar;
 			}
-		}
 		?>
 
 		<div class="author-bio-text">
 			<div class="author-bio-header">
-				<?php
-				if (newspack_is_active_style_pack('style-4')) {
-					$author_avatar = get_avatar(get_the_author_meta('ID'), 80);
-					if ($author_avatar) {
-						// phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
-						echo $author_avatar;
-					}
-				}
-				?>
-
 				<div>
 					<h2 class="accent-header">
 						<?php echo esc_html(get_the_author()); ?>
