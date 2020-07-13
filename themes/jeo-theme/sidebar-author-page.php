5a09a378e2e5142a38d3418f9bc0dc42cdd11555
hockpond <gustavoc.reis@hotmail.com>
2020-07-13 13:56:46 -0300

    Update author page layout

---
+++ themes/jeo-theme/sidebar-author-page.php
@@ -0,0 +1,17 @@
+<aside class="author-page-sidebar">
+    <div>
+        <h4>ABOUT THE AUTHOR</h4>
+        <p><?php echo get_the_author_meta('description'); ?></p>
+
+        <div class="author-most-read">
+            <div class="header">
+                <p>AUTHOR'S MOST READ</p>
+            </div>
+            <div class="posts">
+                <p>Título do conteúdo que geralmente será um título grande</p>
+                <p>Título do conteúdo que geralmente será um título grande</p>
+                <p>Título do conteúdo que geralmente será um título grande</p>
+            </div>
+        </div>
+    </div>
+</aside>
