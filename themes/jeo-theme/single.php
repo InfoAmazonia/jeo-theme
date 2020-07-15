8920e232384fca4e7e057d0b997b8f63e5fcb8f9
Isaque Melo <isaquegbmelo2@gmail.com>
2020-07-15 11:16:30 -0300

    #17

--- themes/jeo-theme/single.php
+++ themes/jeo-theme/single.php
@@ -9,13 +9,23 @@
  */
 
 get_header('single');
-?>
+the_post();
 
-<section id="primary" class="content-area <?php echo esc_attr(newspack_get_category_tag_classes(get_the_ID())) . ' ' . newspack_featured_image_position(); ?>">
-	<?php
-	/* Start the Loop */
-	while (have_posts()) :
-		the_post(); ?>
+$parent_type_category = get_category_by_slug('type')->cat_ID;
+$post_categories = get_the_category();
+$post_child_category = null;
+foreach ($post_categories as $post_cat) {
+	if ($parent_type_category == $post_cat->parent) {
+		$post_child_category = $post_cat;
+		break;
+	}
+}
+
+if ($post_child_category->slug === 'opinion') : ?>
+	<?php get_template_part('template-parts/singles/single', 'opinion'); ?>
+<?php else : ?>
+	<section id="primary" class="content-area <?php echo esc_attr(newspack_get_category_tag_classes(get_the_ID())) . ' ' . newspack_featured_image_position(); ?>">
+
 		<main id="main" class="site-main">
 			<?php
 			$isImageBehind = false;
@@ -79,20 +89,18 @@ get_header('single');
 		<div class="after-post-content-widget-area">
 			<?php dynamic_sidebar('after_post_widget_area'); ?>
 		</div>
-		
-		<div class="main-content">
-		<?php
-		// If comments are open or we have at least one comment, load up the comment template.
-		if (comments_open() || get_comments_number()) {
-			newspack_comments_template();
-		}
 
-		?>
+		<div class="main-content">
+			<?php
+			// If comments are open or we have at least one comment, load up the comment template.
+			if (comments_open() || get_comments_number()) {
+				newspack_comments_template();
+			}
+
+			?>
 		</div>
-	<?php
-	endwhile;
-	?>
-</section><!-- #primary -->
+	</section><!-- #primary -->
+<?php endif; ?>
 
 
 
