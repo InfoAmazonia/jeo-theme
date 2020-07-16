b359f83e892bdc19aab07d84ddac2721d39effef
Isaque Melo <isaquegbmelo2@gmail.com>
2020-07-16 10:09:55 -0300

    Add support to simple video block

--- themes/jeo-theme/assets/javascript/video-repositioning.js
+++ themes/jeo-theme/assets/javascript/video-repositioning.js
@@ -1,5 +1,6 @@
 window.addEventListener("DOMContentLoaded", function () {
-    const allYoutubeBlocks = document.querySelectorAll('.wp-block-embed-youtube');
+    const allYoutubeBlocks = document.querySelectorAll('.wp-block-embed-youtube, .wp-block-video');
+    console.log(allYoutubeBlocks);
     if(allYoutubeBlocks.length) {
         const target = document.querySelector('.entry-header');
         target.appendChild(allYoutubeBlocks[0]);
