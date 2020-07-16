c903eaf5e6bdf2a33184940445d8060051908620
hockpond <gustavoc.reis@hotmail.com>
2020-07-16 13:51:44 -0300

    Separate widgets styles among different files

--- themes/jeo-theme/archive.php
+++ themes/jeo-theme/archive.php
@@ -79,7 +79,7 @@ get_header();
 			<aside class="author-page-sidebar">
     			<div>
 					<h4>ABOUT THE AUTHOR</h4>
-					<p><?php echo get_the_author_meta('description'); ?></p>
+					<p class="about-the-author"><?php echo get_the_author_meta('description'); ?></p>
 					<?php dynamic_sidebar('author_page_sidebar') ?>
 				</div>
 			</aside>		</section><!-- #primary -->
