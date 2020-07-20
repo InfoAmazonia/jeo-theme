943f87b6211b5d231cd0233fc880d0f6fef52219
pame <pamela.rosales@pucp.pe>
2020-07-20 17:47:55 -0300

    format date in Category/Author/Tag page. refs[#77]

--- themes/jeo-theme/inc/template-tags.php
+++ themes/jeo-theme/inc/template-tags.php
@@ -6,7 +6,7 @@
  */
 function newspack_posted_on()
 {
-	$time_string = '<time class="entry-date published updated" datetime="%1$s">%2$s</time>';
+	$time_string = '<time class="entry-date published abc updated" datetime="%1$s">%2$s</time>';
 	if (get_the_time('U') !== get_the_modified_time('U')) {
 		$time_string = '<time class="entry-date published" datetime="%1$s">%2$s</time><time class="updated" datetime="%3$s">%4$s</time>';
 	}
@@ -14,7 +14,7 @@ function newspack_posted_on()
 	$time_string = sprintf(
 		$time_string,
 		esc_attr(get_the_date(DATE_W3C)),
-		esc_html(get_the_date()),
+		esc_html(get_the_date('j F Y')),
 		esc_attr(get_the_modified_date(DATE_W3C)),
 		esc_html(get_the_modified_date())
 	);
@@ -31,7 +31,7 @@ function newspack_posted_on()
 						($diff->format('%h') * 60) +   
 							$diff->format('%i');       
 				if ($minutes >= 30) { ?>
-					<span class="hide-tablet-down">- (Updated on <?= the_modified_date("j F Y \a\\t G:i") ?>)</span>
+					<span class="hide-tablet-down"> (Updated on <?= the_modified_date("j F Y \a\\t G:i") ?>)</span>
 				<?php 
 				}
 
