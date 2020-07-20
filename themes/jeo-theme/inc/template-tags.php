3c5cf6c7980139f0b2da8bc063097618c6766e24
hockpond <gustavoc.reis@hotmail.com>
2020-07-20 15:38:42 -0300

    Set the same font-size for paragraphs and lists. Change the date format on posts [refs: #67]

--- themes/jeo-theme/inc/template-tags.php
+++ themes/jeo-theme/inc/template-tags.php
@@ -21,7 +21,7 @@ function newspack_posted_on()
 
 	if (is_single()) { ?>
 		<div class="post-date">
-			<?php the_date('F j, Y \a\\t G:i') ?>
+			<?php the_date('j F Y \a\\t G:i') ?>
 			<?php if (get_the_date() != get_the_modified_date() || get_the_time() != get_the_modified_time()) : ?>
 				<?php 
 				$posted = new DateTime(get_the_date('c'));
@@ -31,7 +31,7 @@ function newspack_posted_on()
 						($diff->format('%h') * 60) +   
 							$diff->format('%i');       
 				if ($minutes >= 30) { ?>
-					<span class="hide-tablet-down">- (Updated <?= the_modified_date("F j, Y \a\\t G:i") ?>)</span>
+					<span class="hide-tablet-down">- (Updated on <?= the_modified_date("j F Y \a\\t G:i") ?>)</span>
 				<?php 
 				}
 
