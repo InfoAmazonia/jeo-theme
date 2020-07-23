2bfba1f8a6184928873dcdf754c6566e53b3949a
hockpond <gustavoc.reis@hotmail.com>
2020-07-23 13:22:11 -0300

    Fix header for pages with one-column layout [refs:#80]

--- themes/jeo-theme/single-wide.php
+++ themes/jeo-theme/single-wide.php
@@ -10,7 +10,11 @@
  * @package Newspack
  */
 
-get_header('single');
+if(is_single()) {
+	get_header('single');
+} else {
+	get_header();
+}
 
 $parent_type_category = get_category_by_slug('type')->cat_ID;
 $post_categories = get_the_category();
