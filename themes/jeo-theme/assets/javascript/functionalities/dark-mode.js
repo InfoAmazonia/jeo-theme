5985ff0a5582e8381b186946e28f302939d52bda
Isaque Melo <isaquegbmelo2@gmail.com>
2020-08-06 13:13:50 -0300

    Dark mode persistence

--- themes/jeo-theme/assets/javascript/functionalities/dark-mode.js
+++ themes/jeo-theme/assets/javascript/functionalities/dark-mode.js
@@ -1,12 +1,24 @@
 window.addEventListener("DOMContentLoaded", function () {
     if(localStorage.getItem('theme')) {
-        jQuery('html').css('font-size', localStorage.getItem('fontSizeAccessibility') + 'px');
+        if(localStorage.getItem('theme') == "dark") {
+            jQuery('body').addClass('dark-theme');
+        }
+
     } else {
         const userPrefersDark = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches;
         const userPrefersLight = window.matchMedia && window.matchMedia('(prefers-color-scheme: light)').matches;
 
         if(userPrefersDark){
-            console.log("User prefers a dark interface");
+            jQuery('body').addClass('dark-theme');
+            localStorage.setItem('theme', "dark");
+        } 
+        
+        if(userPrefersLight || !userPrefersDark && !userPrefersLight){
+            if(jQuery('body').hasClass('dark-theme')) {
+                jQuery('body').removeClass('dark-theme');
+            }
+
+            localStorage.setItem('theme', "light");
         }
     }
 
@@ -16,5 +28,13 @@ window.addEventListener("DOMContentLoaded", function () {
         jQuery(this.querySelector("i:last-child")).toggleClass(
             "fa-toggle-off fa-toggle-on"
         );
+
+        localStorage.setItem('theme', localStorage.getItem('theme') == "dark"? "light" : "dark");
+
+        if(localStorage.getItem('theme') == "dark") {
+            this.querySelector('.item--title').innerHTML = "Light mode";
+        } else {
+            this.querySelector('.item--title').innerHTML = "Dark mode";
+        }
     });
 });
