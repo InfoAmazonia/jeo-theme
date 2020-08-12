742e9782984a68f5aebf17ac7e8d2882d0c83344
hockpond <gustavoc.reis@hotmail.com>
2020-08-12 12:15:31 -0300

    Improve more menu UX [refs: #119]

--- themes/jeo-theme/assets/javascript/functionalities/header.js
+++ themes/jeo-theme/assets/javascript/functionalities/header.js
@@ -115,6 +115,14 @@ window.addEventListener("DOMContentLoaded", function () {
         jQuery(".more-menu--content").addClass('permahover');
     });
 
+    document.querySelector('.mobile-sidebar').childNodes.forEach( childNode => {
+        childNode.addEventListener("mouseover", () => {
+            if(childNode.className != 'more-menu') {
+                jQuery(".more-menu--content").removeClass('permahover');
+            }
+        })
+    } );
+
     window.addEventListener('click', function(e){   
         if (!document.querySelector('.more-menu--content').contains(e.target) &&
         !document.querySelector('.more-menu').contains(e.target)
