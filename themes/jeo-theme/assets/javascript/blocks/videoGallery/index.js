bcf94c128c03afa1c9b7d250f9a73dba693815ad
Isaque Melo <isaquegbmelo2@gmail.com>
2020-08-12 16:11:49 -0300

    Video galley/Embed template - UI

---
+++ themes/jeo-theme/assets/javascript/blocks/videoGallery/index.js
@@ -0,0 +1,67 @@
+import { RichText, InnerBlocks } from "@wordpress/block-editor";
+
+import { __ } from "@wordpress/i18n";
+import { Button, SelectControl, TextControl } from "@wordpress/components";
+//const {  } = wp.editor;
+
+wp.blocks.registerBlockType("jeo-theme/custom-video-gallery", {
+	title: "Video Gallery",
+	icon: "format-video",
+	category: "common",
+	supports: {
+		align: true,
+	},
+	attributes: {
+		title: {
+			type: "string",
+		},
+	},
+
+	edit: (props) => {
+		const {
+			className,
+			isSelected,
+			attributes: {
+			  title,
+			},
+			setAttributes,
+		} = props;
+		
+		const TEMPLATE =  [['jeo-theme/embed-template']];
+		return (
+			<>
+				<div className="video-gallery-wrapper" key="container">
+					<div>
+						<InnerBlocks
+							allowedBlocks={[ 'jeo-theme/embed-template' ]}
+							template={TEMPLATE}
+						/>
+
+					</div>
+				</div>
+			</>
+		);
+	},
+
+	save: (props) => {
+		const {
+			className,
+			isSelected,
+			attributes: {
+			  title,
+			},
+			setAttributes,
+		  } = props;
+	  
+
+		return (
+			<>
+				<div className="video-gallery-wrapper" key="container">
+					<InnerBlocks.Content/>
+				</div>
+			</>
+		);
+	},
+});
+
+// [mc4wp_form id="65"]
