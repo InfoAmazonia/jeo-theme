4b75211b314ce1bce3125932a11aa8f0df9817a4
hockpond <gustavoc.reis@hotmail.com>
2020-08-13 10:41:20 -0300

    Alert the user of the consequences of deleting native categories of the theme [refs: #128]

--- themes/jeo-theme/assets/javascript/app.js
+++ themes/jeo-theme/assets/javascript/app.js
@@ -5,6 +5,7 @@ import "./functionalities/audio-player";
 import "./functionalities/video-repositioning";
 import "./functionalities/header";
 import "./functionalities/cover-block";
+import "./functionalities/category-deletion-warning";
 import "./cookies";
 import "./image-gallery";
 import "./../vendor/sss/sss.min";
