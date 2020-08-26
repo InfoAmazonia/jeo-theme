fed860c6125064c652aaab57bdd18cf80b2fd9c9
Isaque Melo <isaquegbmelo2@gmail.com>
2020-08-26 13:47:51 -0300

    Fix external link when metaarea is not defined

--- themes/jeo-theme/assets/javascript/app.js
+++ themes/jeo-theme/assets/javascript/app.js
@@ -66,7 +66,15 @@ window.addEventListener("DOMContentLoaded", function () {
                         },
                     });
 
-                    const metaarea = element.closest("article").querySelector('.entry-meta');
+                    let metaarea = element.closest("article").querySelector(".entry-meta");
+                    
+                    if (!metaarea) {
+                        metaarea = document.createElement("div");
+                        metaarea.classList.add("entry-meta");
+
+                        element.closest("article").querySelector(".entry-wrapper").appendChild(metaarea);
+                    }
+
                     metaarea.insertBefore(externalSourceLink, metaarea.firstChild);
                 }
             } catch (err) {
@@ -79,7 +87,7 @@ window.addEventListener("DOMContentLoaded", function () {
 (function ($) {
     jQuery(document).ready(function () {
         // Fix JEO-plugin and Vue conflit.
-        document.querySelectorAll('.vue-component').forEach(function(element) {
+        document.querySelectorAll('.vue-component').forEach(function (element) {
             new Vue({
                 el: element,
             });
@@ -97,8 +105,8 @@ window.addEventListener("DOMContentLoaded", function () {
         }
 
         // prevents comments from hiding when a direct comment hash is set
-        if(!(document.location.hash.length && document.location.hash.slice(1,8) == 'comment')) {
-            jQuery(".toggable-comments-form").hide();   
+        if (!(document.location.hash.length && document.location.hash.slice(1, 8) == 'comment')) {
+            jQuery(".toggable-comments-form").hide();
         }
 
         if (jQuery(".toggable-comments-area").length) {
