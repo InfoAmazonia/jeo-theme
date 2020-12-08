dc10055023356d1ffa979b68ea1d7cbaf41e49c2
hockpond <gustavoc.reis@hotmail.com>
2020-12-08 17:15:22 -0300

    Change cookie banner location and behavior [refs: #301]

--- themes/jeo-theme/assets/javascript/cookies.js
+++ themes/jeo-theme/assets/javascript/cookies.js
@@ -2,8 +2,7 @@ window.addEventListener("DOMContentLoaded", function () {
     const parent = document.querySelector('.cc-window');
     const hasChild = document.querySelector('.cc-window .jeo');
     const acceptAllBtn = document.querySelector('.cc-accept-all');
-    console.log("acceptall");
-    console.log(acceptAllBtn);
+    
     if (parent && !hasChild) {
         const content = parent.innerHTML;
         const additionalElement = document.createElement('div');
@@ -11,5 +10,25 @@ window.addEventListener("DOMContentLoaded", function () {
         additionalElement.innerHTML = content;
         parent.innerHTML = "";
         parent.appendChild(additionalElement);
+    }
+
+    if (!parent.classList.contains('cc-invisible') || parent.style.display != 'none') {
+        document.querySelector('#page').style.background = 'grey';
+        document.querySelector('#page').style.filter = 'brightness(0.5)';
+    }
+
+    const buttons = [document.querySelector('.cc-window a.cc-deny'), document.querySelector('.cc-window a.cc-allow')]
+    buttons.map(button => {
+        button.onclick = () => {
+            document.querySelector('#page').style.background = 'unset';
+            document.querySelector('#page').style.filter = 'unset';
+        }
+    });
+
+    if(document.querySelector('.cc-bottom')) {
+        document.querySelector('.cc-bottom').onclick = () => {
+            document.querySelector('#page').style.background = 'grey';
+            document.querySelector('#page').style.filter = 'brightness(0.5)';
+        }
     }
 })
