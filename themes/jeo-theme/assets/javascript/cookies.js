e6a726f44670a93674682dd8f2f152a7d7f2a636
hockpond <gustavoc.reis@hotmail.com>
2020-12-09 17:52:22 -0300

    .

--- themes/jeo-theme/assets/javascript/cookies.js
+++ themes/jeo-theme/assets/javascript/cookies.js
@@ -29,7 +29,7 @@ window.addEventListener("DOMContentLoaded", function () {
         darkerScreen.style.display = 'block';
     }
 
-    const buttons = document.querySelectorAll('.cc-btn:not(.cc-show-settings)');
+    const buttons = document.querySelectorAll('.cc-compliance .cc-btn:not(.cc-show-settings)');
 
     buttons.forEach((button) => {
         button.addEventListener('click', () => {
