99fd6b4cee251ac1edd50c46490e77554f7e42a6
pame <pamela.rosales@pucp.pe>
2020-12-11 08:00:54 -0300

    remove dist files

--- themes/jeo-theme/dist/app.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('jquery', 'wp-polyfill'), 'version' => '2d5296579634994ac1979c4851edffd3');
--- themes/jeo-theme/dist/newsletter.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-components', 'wp-i18n', 'wp-polyfill'), 'version' => 'a3848669d24890a691453e2a07f885c1');
